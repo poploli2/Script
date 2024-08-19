918cf0cb3102bfacdbbe7f88ebc5d6a236266073
poploli2 <133581727+poploli2@users.noreply.github.com>
2024-08-19 18:39:05 +0800

    Update buy.js

--- xbox/ng/buy.js
+++ xbox/ng/buy.js
@@ -9,11 +9,15 @@ if (jsonData.market) {
     const originalMarket = jsonData.market;
     jsonData.market = 'NG';
     
-    if (jsonData.friendlyName) {
-        const marketRegex = new RegExp(originalMarket + '$');
-        jsonData.friendlyName = jsonData.friendlyName.replace(marketRegex, 'NG');
-    }
+    // if (jsonData.friendlyName) {
+    //     const marketRegex = new RegExp(originalMarket + '$');
+    //     jsonData.friendlyName = jsonData.friendlyName.replace(marketRegex, 'NG');
+    // }
 }
 
+if (jsonData.friendlyName){
+    jsonData.friendlyName = 'cart-save-for-later-NG';
+}
+    
 let newBody = JSON.stringify(jsonData);
 $done({body: newBody});
