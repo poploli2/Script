e33b173ac4e9a6d485c9f07cab14f7e1a6e0323d
poploli2 <133581727+poploli2@users.noreply.github.com>
2024-08-19 19:24:56 +0800

    Update buy.js

--- xbox/ng/buy.js
+++ xbox/ng/buy.js
@@ -1,27 +1,34 @@
+// 修改请求 URL 中的查询参数（params）
+let url = $request.url;
+let urlObj = new URL(url);
+
+// 修改 market 和 locale 的查询参数
+if (urlObj.searchParams.has('market')) {
+    urlObj.searchParams.set('market', 'NG');
+}
+
+if (urlObj.searchParams.has('locale')) {
+    urlObj.searchParams.set('locale', 'en-NG');
+}
+
+// 修改请求体中的 market 和 locale
 let body = $request.body;
-let jsonData = JSON.parse(body);
+if (body) {
+    let jsonData = JSON.parse(body);
 
-if (jsonData.locale) {
-    jsonData.locale = 'en-NG';
+    // 修改请求体中的 market
+    if (jsonData.market) {
+        jsonData.market = 'NG';
+    }
+
+    // 修改请求体中的 locale
+    if (jsonData.locale) {
+        jsonData.locale = 'en-NG';
+    }
+
+    // 将修改后的请求体转换回字符串
+    body = JSON.stringify(jsonData);
 }
 
-if (jsonData.market) {
-    const originalMarket = jsonData.market;
-    jsonData.market = 'NG';
-    
-    // if (jsonData.friendlyName) {
-    //     const marketRegex = new RegExp(originalMarket + '$');
-    //     jsonData.friendlyName = jsonData.friendlyName.replace(marketRegex, 'NG');
-    // }
-}
-
-// if (jsonData.friendlyName){
-//     jsonData.friendlyName = 'cart-save-for-later-NG';
-// }
-
-if (jsonData.friendlyName){
-    jsonData.friendlyName = 'cart-NG';
-}
-
-let newBody = JSON.stringify(jsonData);
-$done({body: newBody});
+// 将修改后的 URL 和请求体应用回请求中
+$done({url: urlObj.toString(), body: body});
