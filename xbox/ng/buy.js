8b7a7400327585523d91190aa840b9a556541578
poploli2 <133581727+poploli2@users.noreply.github.com>
2024-08-20 13:42:41 +0800

    Update buy.js

--- xbox/ng/buy.js
+++ xbox/ng/buy.js
@@ -2,19 +2,17 @@
 
 if ($request.method.toUpperCase() !== "OPTIONS") {
 
-  const $tool = new Tool();
   let headers = $request.headers;
   let requestBody = JSON.parse($request.body);
-  // let requestUrl = $request.url;
 
-  if (jsonData.market) {
-      const originalMarket = jsonData.market;
-      jsonData.market = 'NG';
+  if (requestBody.market) {  
+    const originalMarket = requestBody.market;
+    requestBody.market = 'NG';
 
-      if (jsonData.friendlyName) {
-        const marketRegex = new RegExp(originalMarket + '$');
-        jsonData.friendlyName = jsonData.friendlyName.replace(marketRegex, 'NG');
-      }
+    if (requestBody.friendlyName) {
+      const marketRegex = new RegExp(originalMarket + '$');
+      requestBody.friendlyName = requestBody.friendlyName.replace(marketRegex, 'NG');
+    }
   }
 
   if (requestBody.locale) {
@@ -36,42 +34,4 @@ if ($request.method.toUpperCase() !== "OPTIONS") {
 
 } else {
   $done({});
-}
-
-function Tool() {
-
-  const _node = (() => {
-    if (typeof require === "function") {
-      const request = require("request");
-      return { request: request };
-    } else {
-      return null;
-    }
-  })();
-
-  this.isSurge = typeof $httpClient !== "undefined";
-  this.isQuanX = typeof $task !== "undefined";
-  this.isResponse = typeof $response !== "undefined";
-
-  this.notify = (title, subtitle, message) => {
-    if (this.isQuanX) {
-      $notify(title, subtitle, message);
-    }
-    if (this.isSurge) {
-      $notification.post(title, subtitle, message);
-    }
-    if (_node) {
-      console.log(JSON.stringify({ title: title, subtitle: subtitle, message: message }));
-    }
-  };
-
-  this.write = (value, key) => {
-    if (this.isQuanX) return $prefs.setValueForKey(value, key);
-    if (this.isSurge) return $persistentStore.write(value, key);
-  };
-
-  this.read = (key) => {
-    if (this.isQuanX) return $prefs.valueForKey(key);
-    if (this.isSurge) return $persistentStore.read(key);
-  };
 }
