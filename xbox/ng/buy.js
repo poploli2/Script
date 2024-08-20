d8e6e80149f0c22c530779fd72bd206ecd0cb913
poploli2 <133581727+poploli2@users.noreply.github.com>
2024-08-20 14:55:16 +0800

    Update buy.js

--- xbox/ng/buy.js
+++ xbox/ng/buy.js
@@ -1,36 +1,40 @@
 (function () {})();
 
 if ($request.method.toUpperCase() !== "OPTIONS") {
+  try {
+    let headers = $request.headers;
+    let requestBody = JSON.parse($request.body);
 
-  let headers = $request.headers;
-  let requestBody = JSON.parse($request.body);
+    if (requestBody.market) {  
+      const originalMarket = requestBody.market;
+      requestBody.market = 'NG';
 
-  if (requestBody.market) {  
-    const originalMarket = requestBody.market;
-    requestBody.market = 'NG';
-
-    if (requestBody.friendlyName) {
-      const marketRegex = new RegExp(originalMarket + '$');
-      requestBody.friendlyName = requestBody.friendlyName.replace(marketRegex, 'NG');
-    }
-  }
-
-  if (requestBody.locale) {
-    requestBody.locale = "en-NG";
-  }
-
-  for (let key in headers) {
-    if (headers.hasOwnProperty(key)) {
-      if (key.toLowerCase() === "x-ms-market") {
-        headers[key] = "NG";
+      if (requestBody.friendlyName) {
+        const marketRegex = new RegExp(originalMarket + '$');
+        requestBody.friendlyName = requestBody.friendlyName.replace(marketRegex, 'NG');
       }
     }
-  }
 
-  $done({
-    headers: headers,
-    body: JSON.stringify(requestBody)
-  });
+    if (requestBody.locale) {
+      requestBody.locale = "en-NG";
+    }
+
+    for (let key in headers) {
+      if (headers.hasOwnProperty(key)) {
+        if (key.toLowerCase() === "x-ms-market") {
+          headers[key] = "NG";
+        }
+      }
+    }
+
+    $done({
+      headers: headers,
+      body: JSON.stringify(requestBody)
+    });
+  } catch (error) {
+    console.log("Error:", error.message);
+    $done({});  
+  }
 
 } else {
   $done({});
